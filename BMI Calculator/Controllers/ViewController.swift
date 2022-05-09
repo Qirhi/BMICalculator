e07fe0e5951a175618fbb291fd07a6836a4c4534
Rhiannon Stanford <rhiannonstanford@gmail.com>
2022-05-09 16:58:03 -0700

    Add IBOutlets for height and weight labels.  Format data to needed String data type. Display on UI Labels

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -11,13 +11,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+//    let text = "\(height)m"
+    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         let heightValue = sender.value
-        print( round(heightValue * 100) / 100.0 )
+        let formattedHeightValue = String(format: "%.2f", heightValue) // convert to String
+        self.heightLabel.text = "\(formattedHeightValue)m"
+        
     }
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         let weightValue = sender.value
-        print ( round(weightValue * 100) / 100.0 )
+        let formattedWeightValue = String(Int(weightValue)) // convert to String
+        self.weightLabel.text = "\(formattedWeightValue)Kg"
     }
     
     
